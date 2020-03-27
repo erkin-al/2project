11b9599e225195c0fa3510784dc42f349b2a90c5
Erkin Aliyev <alieverkin@gmail.com>
2020-03-27 16:52:21 +0400

    .

---
+++ script - Copy.js
@@ -0,0 +1,46 @@
+let myButton = document.getElementById("calculation");//Присваиваем кнопке имя
+let wrong = document.getElementById("wrongdata");//Присваиваем имя полю с ошибкой, которое пока скрыто
+
+    function calculateDeposit(start, payment, rate, term) {
+        start = +document.getElementById("startvalue").value; //Даем ссылку на данные для переменной 
+        payment = +document.getElementById("addition").value; //Даем ссылку на данные для переменной
+        rate = +document.getElementById("percent").value;     //Даем ссылку на данные для переменной
+        term = +document.getElementById("term").value;        //Даем ссылку на данные для переменной     
+        wrong.className = "nonmistake";
+        if (start <= 0) {
+            console.log("Ошибка- Начальная сумма должна быть больше нуля.");
+            wrong.className = "mistake";// указываем автоневидмость полю с ошибкой
+        } else if (rate <= 0) {
+            console.log("Ошибка- Процентная ставка должна быть больше нуля и не более 100%.");
+            wrong.className = "mistake";
+        } else if (term <= 0) {
+            console.log("Ошибка- Срок вклада должен быть больше нуля.");
+            wrong.className = "mistake";
+        } else if (payment < 0) {
+            console.log("Ошибка- Сумма допвзноса должена быть больше нуля.");
+            wrong.className = "mistake";
+            // Подсчет по первому месяцу, не учитываем в нем дополнительные взнос, он будет только начиная со второго месяца
+        } else if ((start > 0) && (payment >= 0) && (rate > 0) && (rate <= 100) && (term > 0) && (term <= 30) && (Number.isInteger(term))) {
+            let trueterm = Math.floor(term / 30); //указываем, что подсчеты должны быть по полным месяцам, неполная часть месяца не учитывается
+            for (let i = 0; i < trueterm; i++) //Циклуем
+            {
+                start *= (1 + rate / 100 / 12);
+            }
+            console.log(start);
+            return start;
+            //Подсчеты по последующим месяцам
+        } else if ((start > 0) && (payment >= 0) && (rate > 0) && (rate <= 100) && (term > 30) && (Number.isInteger(term))) {
+            let trueterm = Math.floor(term / 30);
+            for (let i = 0; i < trueterm; i++) {
+                start += payment;//Добавляем допвзнос
+                start *= (1 + rate / 100 / 12);
+            }
+            console.log(start);
+            return start;
+        } else {             //если İF не выполнился
+
+            wrong.className = "mistake";
+        }
+    }
+    myButton.addEventListener("click", calculateDeposit);//Запускаем кнопку и вызываем ею функцию выше
+    
--- script copy.js
+++ script copy.js
@@ -1,13 +1,13 @@
 let myButton = document.getElementById("calculation");//Присваиваем кнопке имя
 let wrong = document.getElementById("wrongdata");//Присваиваем имя полю с ошибкой, которое пока скрыто
-let start = +document.getElementById("startvalue").value; //Даем ссылку на данные для переменной 
+let start = {}; //Даем ссылку на данные для переменной 
 let payment = +document.getElementById("addition").value; //Даем ссылку на данные для переменной
 let rate = +document.getElementById("percent").value;     //Даем ссылку на данные для переменной
 let term = +document.getElementById("term").value;
 myButton.addEventListener("click", checkValues);
 //Запускаем кнопку и вызываем ею функцию
 
-function origination() {
+function initialize() {
     start = +document.getElementById("startvalue").value; //Даем ссылку на данные для переменной 
     payment = +document.getElementById("addition").value; //Даем ссылку на данные для переменной
     rate = +document.getElementById("percent").value;     //Даем ссылку на данные для переменной
@@ -15,14 +15,14 @@ function origination() {
 }
 
 function checkValues() {
-    origination();
-
+    initialize();
+    
     wrong.className = "nonmistake"; // указываем автоневидмость полю с ошибкой
     if (start <= 0) {
         console.log("Ошибка- Начальная сумма должна быть больше нуля.");
         wrong.className = "mistake";
         return NaN;
-    } else if ((rate <= 0) && (rate > 100)) {
+    } else if (rate <= 0) {
         console.log("Ошибка- Процентная ставка должна быть больше нуля и не более 100%.");
         wrong.className = "mistake";
         return NaN;
@@ -42,20 +42,32 @@ function checkValues() {
 
 
 function calculateDeposit() {
+    
     wrong.className = "nonmistake";
-    //Подсчеты по первому месяцу: 
-    if (term < 29) {
-        return start;
-    }
-    if ((term >= 30) && (Number.isInteger(term))) {
+
+    //Подсчеты по первому месяцу:  
+    if ((rate <= 100) && ((Math.floor(term / 30)) <= 30) && (Number.isInteger(term))) {
+        start *= (1 + rate / 100 / 12);
+        console.log(start);
+
+
+        //Подсчеты по последующим месяцам:
+    } else if ((rate <= 100) && (term > 30) && (Number.isInteger(term))) {
         let trueterm = Math.floor(term / 30);
         for (let i = 0; i < trueterm; i++) {
             start += payment;//Добавляем допвзнос
             start *= (1 + rate / 100 / 12);
         }
-    } else {
+        console.log(start);
+
+    } else {             //если İF не выполнился
+
         wrong.className = "mistake";
     }
+
     return start;
 };
 
+// Основная программа
+
+//checkValues(myStart, myPayment, myRate, myTerm);
